3173112663704e6d61de0c1483dd4d65dff1eade
5101324 <5101324@LHS-PRLAB-37.lhs.win.fuhsd.org>
2014-05-07 10:01:07 -0700

    finished Piece and added PieceDisplay

--- src/Piece.java
+++ src/Piece.java
@@ -1,9 +1,54 @@
+import java.awt.Color;
+
+
+/**
+ * This represents a single piece on the board
+ * 
+ * @author 5101324
+ * @version May 7, 2014
+ * @author Period: 2
+ * @author Assignment: Connect4
+ * 
+ * @author Sources: None
+ */
 public class Piece
 {
-    public static void main(String[] args)
+
+    private Color color;
+
+
+    /**
+     * This is the constructor
+     * 
+     * @param c
+     *            this sets the color of it
+     */
+    public Piece( Color c )
     {
-        System.out.println("Hello world!");
-        System.out.println("Goodbye");
-        System.out.println("poop");
+        color = c;
+    }
+
+
+    /**
+     * Gets the color of the piece.
+     * 
+     * @return the color of the piece
+     */
+    public Color getColor()
+    {
+        return color;
+    }
+
+
+    /**
+     * Sets the color of the piece. Only used in case we use different rules
+     * from Connect4
+     * 
+     * @param newColor
+     *            the new color for the piece
+     */
+    public void setColor( Color newColor )
+    {
+        color = newColor;
     }
 }
