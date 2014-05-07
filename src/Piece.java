7559082d5aff4663fb7c14cdcd489c9b1c3d61b6
5101324 <5101324@LHS-PRLAB-37.lhs.win.fuhsd.org>
2014-05-07 09:35:24 -0700

    changed name of piece to Piece

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,9 @@
-public class piece
+public class Piece
 {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
         System.out.println("Goodbye");
+        System.out.println("poop");
     }
 }
