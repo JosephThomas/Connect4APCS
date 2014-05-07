a40041036a14220aa104286b95e78de5eabf85b5
5101324 <5101324@LHS-PRLAB-37.lhs.win.fuhsd.org>
2014-05-07 10:34:07 -0700

    finished PieceDisplay

--- src/PieceDisplay.java
+++ src/PieceDisplay.java
@@ -3,10 +3,8 @@ import java.awt.Color;
 import java.awt.Shape;
 import java.awt.Component;
 import java.awt.Graphics2D;
-import java.awt.GradientPaint;
 import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.AffineTransform;
 
 /**
  * PieceDisplay.java
@@ -16,9 +14,6 @@ import java.awt.geom.AffineTransform;
 public class PieceDisplay extends AbstractDisplay
 {
     private static final double PIECE_DIAMETER = .8;
-    private static final int GRADIENT_SIZE = 50;
-    private static final AffineTransform ATX =
-        AffineTransform.getScaleInstance(GRADIENT_SIZE, GRADIENT_SIZE);
 
     private Shape body;
 
@@ -27,34 +22,28 @@ public class PieceDisplay extends AbstractDisplay
      */
     public PieceDisplay()
     {
-        double pieceDiameter = PIECE_DIAMETER;
+    	double pieceDiameter = PIECE_DIAMETER;
         float halfLength = (float)pieceDiameter / 2;
 
         body = new Ellipse2D.Double(-halfLength, -halfLength,
                                     pieceDiameter, pieceDiameter);
     }
 
-    /**
-     * Draw a piece.
-     * @param obj the piece to draw
-     * @param comp not used
-     * @param g2 the graphics 2D object to draw with
-     */
+	/**
+	 * Draw a piece.
+	 * @param obj the piece to draw
+	 * @param comp not used
+	 * @param g2 the graphics 2D object to draw with
+	 */
     public void draw(Object obj, Component comp, Graphics2D g2)
     {
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                            RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+						    RenderingHints.VALUE_ANTIALIAS_ON);
 
-        Piece piece = (Piece)obj;
+    	Piece piece = (Piece)obj;
         Color pieceColor = piece.getColor();
         g2.setPaint(pieceColor);
         g2.draw(body);
-
-        g2.scale(1.0 / GRADIENT_SIZE, 1.0 / GRADIENT_SIZE);
-        g2.setPaint(new GradientPaint(
-            -GRADIENT_SIZE / 4, -GRADIENT_SIZE / 2, Color.white,
-            GRADIENT_SIZE / 4, GRADIENT_SIZE / 4, pieceColor));
-        g2.fill(ATX.createTransformedShape(body));
-        g2.scale(GRADIENT_SIZE, GRADIENT_SIZE);
+        g2.fill(body);
     }
 }
